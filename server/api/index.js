7ee94b5b2cc72e9bd3d9569d0cb76c9822c5688b
Tsumuraya <tsumuraya@xscore.co.jp>
2019-03-26 18:21:23 +0900

    register

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 
 import postItem from './posts'
+import register from './register'
 const router = Router()
 
 router.use(postItem)
+router.use(register)
 export default router
